2ddd41dcb5eb31a0b5493f05c7ace507baf34fbb
Andrey-Kolomiychuk <101461917+Andrey-Kolomiychuk@users.noreply.github.com>
2022-07-15 14:21:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        // autoplay: true,
+        // autoplayspeed: 1000
+    });
+  });
+
+
+$(document).ready(function(){
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 1000
+    });
+  });
